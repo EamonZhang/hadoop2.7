9b9fc71cd0745baf6c5f3d65bd3573916b6e782f
eamon <zhang.eamon@hotmail.com>
2016-08-26 16:13:48 +0800

    add hbase put test

---
+++ src/com/bodani/hbase/PutExample.java
@@ -0,0 +1,43 @@
+package com.bodani.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.bodani.hbase.util.HBaseHelper;
+
+import java.io.IOException;
+
+public class PutExample {
+
+  public static void main(String[] args) throws IOException {
+    Configuration conf = HBaseConfiguration.create(); // co PutExample-1-CreateConf Create the required configuration.
+    conf.set("hbase.zookeeper.quorum", "v1,v2,v3");// zookeeper集群地址
+    // ^^ PutExample
+    HBaseHelper helper = HBaseHelper.getHelper(conf);
+    helper.dropTable("testtable");
+    helper.createTable("testtable", "colfam1");
+    // vv PutExample
+    Connection connection = ConnectionFactory.createConnection(conf);
+    Table table = connection.getTable(TableName.valueOf("testtable")); // co PutExample-2-NewTable Instantiate a new client.
+
+    Put put = new Put(Bytes.toBytes("row1")); // co PutExample-3-NewPut Create put with specific row.
+
+    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
+      Bytes.toBytes("val1")); // co PutExample-4-AddCol1 Add a column, whose name is "colfam1:qual1", to the put.
+    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
+      Bytes.toBytes("val2")); // co PutExample-4-AddCol2 Add another column, whose name is "colfam1:qual2", to the put.
+
+    table.put(put); // co PutExample-5-DoPut Store row with column into the HBase table.
+    table.close(); // co PutExample-6-DoPut Close table and connection instances to free resources.
+    connection.close();
+    // ^^ PutExample
+    helper.close();
+    // vv PutExample
+  }
+}
